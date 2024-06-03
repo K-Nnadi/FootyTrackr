21f16811ca3a03b6c5bbc8a20b4dfe65bfe627ba
kenneth_nnadi@aol.co.uk <kenneth_nnadi@aol.co.uk>
2024-06-03 22:37:35 +0100

    backend dir

--- src/main.ts
+++
@@ -1,5 +0,0 @@
-import {AppModule} from "./app.module";
-import {GenericBootstrap} from "@fl/base-tools/bootstrap/generic.bootstrap";
-require('dotenv').config()
-
-void GenericBootstrap(AppModule, 3000);
