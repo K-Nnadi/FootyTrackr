efb39ee3eabd7425138dd3b09b17b1c2e4bafa64
kenneth_nnadi@aol.co.uk <kenneth_nnadi@aol.co.uk>
2024-09-25 00:47:04 +0100

    migration

--- backend/clientGen/orval.api.config.js
+++ backend/clientGen/orval.api.config.js
@@ -1,6 +1,25 @@
 module.exports = {
-    apiClient: {
-        input: './petstore.yaml', //swagger
-        output: './src/petstore.ts',
-    },
-};
+    mainApi: {
+        output: {
+            mode: 'tags-split',
+            target: '../link-tracker-client/main.ts',
+            client: 'react-query',
+            override: {
+                /*operationName: (operation, route, verb) => {
+                    if(operation?.description){
+                        return operation?.description.replace('Entity', '')
+                    } else {
+                        return operation.operationId
+                    }
+                },*/
+                mutator: {
+                    path: '../../clients/client-instance.ts',
+                    name: 'clientInstance',
+                }
+            }
+        },
+        input: {
+            target: '../openapi.json'
+        }
+    }
+}
